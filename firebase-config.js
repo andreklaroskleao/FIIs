8bca74902655832d474e5da8810ddc1ed09f5086
andreklaroskleao <andre.klar.leao@gmail.com>
2026-03-19 18:08:37 -0300

    Atualizar o firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,4 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js';
 import {
     getAuth,
     GoogleAuthProvider,
@@ -9,32 +8,31 @@ import {
     setPersistence,
     browserLocalPersistence
 } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD4Yq5QBh84yox8-yLS6mxqP3o0vpHQUb4",
-  authDomain: "appfiis-ii.firebaseapp.com",
-  projectId: "appfiis-ii",
-  storageBucket: "appfiis-ii.firebasestorage.app",
-  messagingSenderId: "735639659271",
-  appId: "1:735639659271:web:14174f2a9acb9eefd95ba2",
-  measurementId: "G-DC17FH54CE"
+    apiKey: 'COLOQUE_AQUI_SUA_API_KEY',
+    authDomain: 'COLOQUE_AQUI_SEU_AUTH_DOMAIN',
+    projectId: 'COLOQUE_AQUI_SEU_PROJECT_ID',
+    storageBucket: 'COLOQUE_AQUI_SEU_STORAGE_BUCKET',
+    messagingSenderId: 'COLOQUE_AQUI_SEU_MESSAGING_SENDER_ID',
+    appId: 'COLOQUE_AQUI_SEU_APP_ID'
 };
 
-const aplicacaoFirebase = initializeApp(configuracaoFirebase);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const provider = new GoogleAuthProvider();
 
-const bancoDeDados = getFirestore(aplicacaoFirebase);
-const autenticacao = getAuth(aplicacaoFirebase);
-const provedorGoogle = new GoogleAuthProvider();
-
-provedorGoogle.setCustomParameters({
+provider.setCustomParameters({
     prompt: 'select_account'
 });
 
-export const db = bancoDeDados;
-export const auth = autenticacao;
-export const provider = provedorGoogle;
-
 export {
+    app,
+    db,
+    auth,
+    provider,
     signInWithPopup,
     signOut,
     onAuthStateChanged,
