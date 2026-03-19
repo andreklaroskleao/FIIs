45627fdc1c33762d62933a805c3446885127952f
andreklaroskleao <andre.klar.leao@gmail.com>
2026-03-19 19:43:52 -0300

    Atualizar o store.js

--- state/store.js
+++ state/store.js
@@ -1,21 +1,28 @@
 export const estadoAplicacao = {
     usuarioAtual: null,
-    identificadorAtivoEmEdicao: null,
-    identificadorProventoEmEdicao: null,
-    identificadorAporteEmEdicao: null,
-    filtroSegmentoAtual: 'Todos',
-    filtroInteligenteAtual: 'todos',
-    ordenacaoCarteiraAtual: 'maior-posicao',
-    modoPrivacidadeAtivo: false,
-    instanciaGraficoProventos: null,
-    instanciaGraficoSegmentos: null,
+
     listaAtivosEmMemoria: [],
     listaProventosEmMemoria: [],
     listaAportesEmMemoria: [],
+
+    identificadorAtivoEmEdicao: null,
+    identificadorProventoEmEdicao: null,
+    identificadorAporteEmEdicao: null,
+
+    filtroSegmentoAtual: 'Todos',
+    filtroInteligenteAtual: 'todos',
+    ordenacaoCarteiraAtual: 'maior-posicao',
+
     listaAtivosSelecionadosParaComparacao: [],
-    mapaObservacoesWatchlist: {},
     mapaLinhasExpandidas: {},
+    mapaObservacoesWatchlist: {},
+
+    modoPrivacidadeAtivo: false,
+
     cancelarInscricaoAtivos: null,
     cancelarInscricaoProventos: null,
-    cancelarInscricaoAportes: null
+    cancelarInscricaoAportes: null,
+
+    instanciaGraficoProventos: null,
+    instanciaGraficoSegmentos: null
 };
