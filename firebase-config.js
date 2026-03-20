605556c6db7ea98fc2062aed4439a7ae88691fca
andreklaroskleao <andre.klar.leao@gmail.com>
2026-03-20 09:45:28 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -10,21 +10,17 @@ import {
 } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js';
 
-/*
-    Substitua pelos dados reais do seu projeto Firebase.
-    Mantenha os nomes exatamente assim para compatibilidade total com o restante do projeto.
-*/
-
 const firebaseConfig = {
-  apiKey: "AIzaSyD4Yq5QBh84yox8-yLS6mxqP3o0vpHQUb4",
-  authDomain: "appfiis-ii.firebaseapp.com",
-  projectId: "appfiis-ii",
-  storageBucket: "appfiis-ii.firebasestorage.app",
-  messagingSenderId: "735639659271",
-  appId: "1:735639659271:web:14174f2a9acb9eefd95ba2",
-  measurementId: "G-DC17FH54CE"
+    apiKey: 'AIzaSyD4Yq5QBh84yox8-yLS6mxqP3o0vpHQUb4',
+    authDomain: 'appfiis-ii.firebaseapp.com',
+    projectId: 'appfiis-ii',
+    storageBucket: 'appfiis-ii.firebasestorage.app',
+    messagingSenderId: '735639659271',
+    appId: '1:735639659271:web:14174f2a9acb9eefd95ba2',
+    measurementId: 'G-DC17FH54CE'
 };
-const aplicacaoFirebase = initializeApp(Firebaseconfig);
+
+const aplicacaoFirebase = initializeApp(firebaseConfig);
 const db = getFirestore(aplicacaoFirebase);
 const auth = getAuth(aplicacaoFirebase);
 const provider = new GoogleAuthProvider();
